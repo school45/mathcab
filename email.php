265d5517331c8e496495ffeaa31aab0c0086b68d
school45 <school45.2023@gmail.com>
2022-10-31 00:07:48 +0300

    update

--- email.php
+++ email.php
@@ -10,6 +10,6 @@ $message = $_POST['message'];
 $mes = "Имя: $name \nE-mail: $email \nТема: $header \nТекст: $message";
 // Пытаемся отправить письмо по заданному адресу
 // Если нужно, чтобы письма всё время уходили на ваш адрес — замените первую переменную $email на свой адрес электронной почты
-$send = mail("ipatiy.potey@mail.ru", $header, $mes, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+mail("ipatiy.potey@mail.ru", $header, $mes, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
 // Если отправка прошла успешно — так и пишем
 ?>
